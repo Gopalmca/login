afa8fc07a1c23f52a1963be36b9f51efcbef7bb6
Gopalmca <32816987+Gopalmca@users.noreply.github.com>
2019-07-10 17:34:10 +0530

    student login

---
+++ src/main/java/com/student/controller/StudentController.java
@@ -0,0 +1,29 @@
+package com.student.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.student.TO.User;
+
+
+
+@Controller
+public class StudentController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "home";
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("user-data");
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+
+}
